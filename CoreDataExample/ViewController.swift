a2defb20162ab0e3f8c7ab058cef41e5cf39958e
Subhojit <subhojit@subhojits-macbook-air.local>
2019-05-15 23:01:59 +0530

    First commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Subhojit on 15/05/19.
+//  Copyright © 2019 Subhojit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
